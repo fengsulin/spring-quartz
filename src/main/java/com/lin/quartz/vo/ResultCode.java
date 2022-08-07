8742f5eb2571a629cbca6f1c383f1c8bb246e190
fengsulin <fengsu_lin@163.com>
2022-08-07 23:16:22 +0800

    授权异常枚举

--- src/main/java/com/lin/quartz/vo/ResultCode.java
+++ src/main/java/com/lin/quartz/vo/ResultCode.java
@@ -7,7 +7,9 @@ public enum ResultCode implements StatusCode{
     FAILED(1001,"请求失败"),
     VALIDATE_ERROR(1002,"参数校验失败"),
     RESPONSE_PACK_ERROR(1003,"response返回包装失败"),
-    ERROR(500,"系统错误，请联系管理");
+    ERROR(500,"系统错误，请联系管理"),
+    AUTHENTICATION_ERROR(403,"授权失败");
+
     private int code;
     private String msg;
 
